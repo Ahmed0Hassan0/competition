e2a8f2c5b7aeb4d18708391b471773691c950e69
Ahmed0Hassan0 <111722487+Ahmed0Hassan0@users.noreply.github.com>
2024-01-23 06:40:34 +0200

    Update competition.py

--- competition.py
+++ competition.py
@@ -11,12 +11,11 @@ df = pd.read_csv('Africa 2024-.csv')
 photos_folder = 'photos'
 
 # Display images and results in the Streamlit app
-st.header('Competition Results')
-st.markdown("<hr>", unsafe_allow_html=True)
+st.header('Competition Results', divider='rainbow')
 
 # Display contestant photos and results in the same row
 for index, row in df.iterrows():
-    col1, col2, col3 = st.columns([1, 2, 3])  # Divide the row into 3 parts
+    col1, col2, col3, col4 = st.columns(4, gap='large')  # Divide the row into 3 parts
     
     # Convert the 'Photo_URL' value to string and construct the full path to the image
     image_path = os.path.join(photos_folder, str(row['Photo_URL']))
@@ -25,6 +24,6 @@ for index, row in df.iterrows():
         col1.image(image_path, width=200)
     except Exception as e:
         col1.error(f"Error loading image for {row['Contestant']}: {str(e)}")
-
-    col2.subheader(f"{index + 1}. {row['Contestant']} ")
-    col3.subheader(f"Score: {row['Score']}")
+    with col2:
+        st.info(f"{index + 1}. {row['Contestant']} ")
+        st.metric(label=f":red[_Score:_] :blue[_{row['Score']}_]", value='')
